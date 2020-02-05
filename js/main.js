c006410aa92fde34f333d96e5362bc4ea2a697c0
Brandon Cantello <bcantello@gmail.com>
2020-02-04 16:51:31 -0800

    Implements js functions checkForMatch() and flipCard()

--- js/main.js
+++ js/main.js
@@ -1,15 +1,23 @@
 let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
 
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-let cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2) {
+function checkForMatch() {
     if (cardsInPlay[0] === cardsInPlay[1]) {
         alert("You found a match!");
     } else {
         alert("Your cards did not match. Try again.");
     }
 }
+
+function flipCard(cardId) {
+    console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
+    if (cardsInPlay.length === 2) {
+        checkForMatch();
+    } else {
+        alert("Flip another card.")
+    }
+}
+
+flipCard(0);
+flipCard(1);
