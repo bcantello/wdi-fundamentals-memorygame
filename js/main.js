d6b53ef0189f7b22e1130b1dcfafc6b6b15be992
Brandon Cantello <bcantello@gmail.com>
2020-02-07 00:54:59 -0800

    Adds random card placement, reset button, and outcome message injection

--- js/main.js
+++ js/main.js
@@ -22,32 +22,78 @@ let cards = [
 ];
 
 let cardsInPlay = [];
+let randomOrder = [];
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+function generateRandomCardOrder() {
+    while (randomOrder.length < cards.length) {
+        let randomInt = getRandomInt(4);
+        if (randomOrder.includes(randomInt) !== true) {
+            randomOrder.push(randomInt);
+        }
+    }
+}
 
 function createBoard() {
-    for (let i = 0; i < cards.length; i++) {
+    for (let i = 0; i < randomOrder.length; i++) {
         let cardElement = document.createElement('img');
         cardElement.setAttribute('src', 'images/back.png');
-        cardElement.setAttribute('data-id', i);
+        cardElement.setAttribute('data-id', randomOrder[i]);
         cardElement.addEventListener('click', flipCard);
         document.getElementById('game-board').appendChild(cardElement);
     }
 }
 
+/*Reset button is not currently working correctly. It works fine the first time, however it seems to get
+* stuck in one configuration after it's initial use.*/
+
 function checkForMatch() {
     if (cardsInPlay[0] === cardsInPlay[1]) {
-        alert("You found a match!");
+        responseMessage("You found a match!");
+        let resetButton = document.getElementById('reset-button');
+        resetButton.addEventListener('click', resetAfterWin);
     } else {
-        alert("Your cards did not match. Try again.");
+        responseMessage("Shoot! You did not find a match. Try again!");
+        let resetButton = document.getElementById('reset-button');
+        resetButton.addEventListener('click', resetAfterLoss);
     }
 }
 
 function flipCard() {
     let cardId = this.getAttribute('data-id');
     cardsInPlay.push(cards[cardId].rank);
-    this.setAttribute('src', cards[cardId].cardImage);
-    if (cardsInPlay.length === 2) {
-        checkForMatch();
+    if (cardsInPlay.length < 3) {
+        this.setAttribute('src', cards[cardId].cardImage);
+        if (cardsInPlay.length === 1) {
+            responseMessage("You found a " + cards[cardId].rank + "! Can you find the other one?")
+        } else if (cardsInPlay.length === 2) {
+            checkForMatch();
+        }
+    } else {
+        responseMessage("No Cheating ;)");
     }
 }
 
+function responseMessage(message) {
+    document.getElementById('response-message').innerHTML = message;
+}
+
+function resetAfterWin() {
+    document.getElementById('game-board').innerHTML = "";
+    cardsInPlay = [];
+    randomOrder = [];
+    generateRandomCardOrder();
+    createBoard();
+}
+
+function resetAfterLoss() {
+    document.getElementById('game-board').innerHTML = "";
+    cardsInPlay = [];
+    createBoard();
+}
+
+generateRandomCardOrder();
 createBoard();
